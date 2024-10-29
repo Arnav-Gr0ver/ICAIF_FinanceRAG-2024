2aafc86c0e4f6504f813d3ad14243f015401c910
Arnav-Gr0ver <arnavgrover@gmail.com>
2024-10-29 01:18:06 -0400

    update colBERT results saved
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from ragatouille import RAGPretrainedModel
 from datasets import load_dataset
+import pandas as pd
 
 RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
-finDER_dataset = load_dataset("Linq-AI-Research/FinanceRAG", "FinDER", split="corpus")
+tasks = ["ConvFinQA", "FinDER", "FinQA", "FinQABench", "FinanceBench", "MultiHiertt", "TATQA"]
 
-print(finDER_dataset)
+results_data = []
 
-# finDER_index = RAG.index(
-#     index_name="finDER task corpus index"
-#     collection=,
-#     document_ids=
-# )
+for task in tasks:
+    corpus_dataset = load_dataset("Linq-AI-Research/FinanceRAG", task, split="corpus")
+    query_dataset = load_dataset("Linq-AI-Research/FinanceRAG", task, split="queries")
+
+    corpus_df = pd.DataFrame(corpus_dataset)  
+    unique_corpus_df = corpus_df.drop_duplicates(subset="_id")
+
+    index = RAG.index(
+        collection=unique_corpus_df["text"].tolist(),
+        document_ids=unique_corpus_df["_id"].tolist()
+    )
+
+    for i in range(len(query_dataset)):
+        query_id = query_dataset[i]["_id"]
+        query = query_dataset[i]["text"]
+        results = RAG.search(query)
+
+        for result in results:
+            results_data.append({
+              "query_id": query_id, 
+              "result_id": result["document_id"],
+            })
+        print(i)
+
+results_df = pd.DataFrame(results_data)
+results_df.to_csv("results/rag_results.csv", index=False, encoding="utf-8")
