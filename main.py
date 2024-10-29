17b31a758bb8a15de8b63d151c7b5f13f73fc182
Arnav-Gr0ver <arnavgrover@gmail.com>
2024-10-28 20:55:11 -0400

    add ragatoullie colBERT
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- main.py
+++ main.py
@@ -1,76 +1,14 @@
-from sentence_transformers import CrossEncoder
-import logging
-import os
-import pandas as pd
+from ragatouille import RAGPretrainedModel
+from datasets import load_dataset
 
-from financerag.rerank import CrossEncoderReranker
-from financerag.retrieval import DenseRetrieval, SentenceTransformerEncoder
-from financerag.tasks import (
-    ConvFinQA,
-    FinDER,
-    FinQABench,
-    FinanceBench,
-    MultiHiertt,
-    TATQA,
-)
+RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
-logging.basicConfig(level=logging.INFO)
+finDER_dataset = load_dataset("Linq-AI-Research/FinanceRAG", "FinDER", split="corpus")
 
-tasks = {
-    "FinDER": FinDER(),
-    "ConvFinQA": ConvFinQA(),
-    "FinQABench": FinQABench(),
-    "FinanceBench": FinanceBench(),
-    "MultiHiertt": MultiHiertt(),
-    "TATQA": TATQA()
-}
+print(finDER_dataset)
 
-encoder_model = SentenceTransformerEncoder(
-    model_name_or_path='intfloat/e5-large-v2',
-    query_prompt='query: ',
-    doc_prompt='passage: ',
-)
-
-retrieval_model = DenseRetrieval(
-    model=encoder_model
-)
-
-reranker = CrossEncoderReranker(
-    model=CrossEncoder('cross-encoder/ms-marco-MiniLM-L-12-v2')
-)
-
-for task_name, task in tasks.items():
-    logging.info(f"Processing task: {task_name}")
-
-    retrieval_result = task.retrieve(
-        retriever=retrieval_model
-    )
-
-    reranking_result = task.rerank(
-        reranker=reranker,
-        results=retrieval_result,
-        top_k=100,
-        batch_size=32
-    )
-
-    output_dir = './results'
-    task.save_results(output_dir=output_dir)
-
-    task_results_file = os.path.join(output_dir, f'{task_name}.csv')
-
-results_folder = 'results'
-
-dataframes = []
-
-for root, _, files in os.walk(results_folder):
-    for file in files:
-        if file == 'results.csv':
-            file_path = os.path.join(root, file)
-            df = pd.read_csv(file_path)
-            dataframes.append(df)
-
-combined_df = pd.concat(dataframes, ignore_index=True)
-
-combined_df.to_csv('combined_results.csv', index=False)
-
-logging.info("All results.csv files have been successfully concatenated into combined_results.csv")
+# finDER_index = RAG.index(
+#     index_name="finDER task corpus index"
+#     collection=,
+#     document_ids=
+# )
