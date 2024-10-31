9cb4b86570ad98d0349144ea8fec07a6f93b5a19
Arnav-Gr0ver <arnavgrover@gmail.com>
2024-10-30 22:18:38 -0400

    update dataset preprocessing
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- train.py
+++ train.py
@@ -5,7 +5,11 @@ qa_dataset = load_dataset("FinGPT/fingpt-fiqa_qa", split="train")
 
 trainer = RAGTrainer(model_name = "FinColBERT", pretrained_model_name = "colbert-ir/colbertv2.0")
 
-trainer.prepare_training_data(raw_data=qa_dataset, data_out_path="./data/checkpoints/")
+qa_pairs = [(qa_dataset["input"][i], qa_dataset["output"][i]) for i in range(len(qa_dataset))]
+
+print(qa_pairs)
+
+trainer.prepare_training_data(raw_data=qa_pairs, data_out_path="./data/checkpoints/")
 
 trainer.train(batch_size=32)
 
